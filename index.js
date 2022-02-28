b0cb891f48a4d4d0034f8c6b23157effb994679c
abdanbarkaath <dj.abdan@gmail.com>
2022-02-28 19:17:54 +0530

    Added database and connected database with node

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
+
 var app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//Routes
+//craete todo
+app.post("/todo", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+    res.send("new todo added successfully");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//get all todos
+app.get("/todos", async (req, res) => {
+  console.log("asdasdas");
+  const allTodos = await pool.query("SELECT * FROM todo");
+  res.json(allTodos.rows);
+});
 
 app.listen(5000, () => {
   console.log("port running on 5000");
